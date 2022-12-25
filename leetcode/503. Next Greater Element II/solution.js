ff33936c5b49c104e0b6f4d8920d81ffad65b454
Thanaphoom Babparn <tp.babparn@gmail.com>
2022-12-25 20:48:00 +0900

    Add image 503

---
+++ leetcode/503. Next Greater Element II/solution.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var nextGreaterElements = function (nums) {
+  const doubleNums = [...nums, ...nums];
+  const length = nums.length;
+  const result = Array(nums.length).fill(-1);
+  const stack = [];
+  for (let i = 0; i < doubleNums.length; i++) {
+    // Monotonic condition control
+    while (
+      stack.length !== 0 &&
+      doubleNums[i] > doubleNums[stack[stack.length - 1]]
+    ) {
+      const index = stack.pop() % length;
+      result[index] = doubleNums[i];
+    }
+    stack.push(i);
+  }
+  return result;
+};
