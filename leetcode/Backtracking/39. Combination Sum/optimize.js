dca411dfd8509cc58b080fbcf8f93951850c73ec
Thanaphoom Babparn <tp.babparn@gmail.com>
2022-12-29 13:59:13 +0900

    Add 39 optimize

---
+++ leetcode/Backtracking/39. Combination Sum/optimize.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+var combinationSum = function (candidates, target) {
+  // result, initialState, target
+  const result = [];
+  backtracking(candidates, 0, result, target, []);
+  return result;
+};
+
+const backtracking = (candidates, index, result, remain, state) => {
+  // Goal
+  if (index === candidates.length) {
+    return;
+  }
+  if (remain === 0) {
+    result.push([...state]);
+    return;
+  }
+  // Choice
+  for (let i = index; i < candidates.length; i++) {
+    const n = candidates[i];
+    // Move computation check before push to function call stack instead
+    if (remain - n < 0) {
+      return;
+    }
+    state.push(n);
+    // Constraint
+    backtracking(candidates, i, result, remain - n, state);
+    state.pop();
+  }
+};
