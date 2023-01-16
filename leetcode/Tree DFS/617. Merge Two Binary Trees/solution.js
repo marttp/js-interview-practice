e62abb3e67ee0bf9d14e9d5e9ac17a38a51667ef
Thanaphoom Babparn <tp.babparn@gmail.com>
2023-01-16 21:52:23 +0900

    Add solution 617

---
+++ leetcode/Tree DFS/617. Merge Two Binary Trees/solution.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root1
+ * @param {TreeNode} root2
+ * @return {TreeNode}
+ */
+var mergeTrees = function (root1, root2) {
+  if (root1 === null && root2 === null) {
+    return null;
+  } else if (root1 === null) {
+    return root2;
+  } else if (root2 === null) {
+    return root1;
+  }
+  root1.val += root2.val;
+  root1.left = mergeTrees(root1.left, root2.left);
+  root1.right = mergeTrees(root1.right, root2.right);
+  return root1;
+};
