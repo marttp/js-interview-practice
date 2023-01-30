ae4486f2d4dec3856c14305f0058c44717f8d567
Thanaphoom Babparn <tp.babparn@gmail.com>
2023-01-31 02:00:59 +0900

    Add 451

---
+++ leetcode/Top K Elements/451. Sort Characters By Frequency/solution.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var frequencySort = function (s) {
+  const freqMap = {};
+  for (const c of s) {
+    if (!(c in freqMap)) freqMap[c] = 0;
+    freqMap[c] += 1;
+  }
+  const pq = new MaxPriorityQueue({ priority: (e) => e[1] });
+  for (const entry of Object.entries(freqMap)) {
+    pq.enqueue(entry);
+  }
+  // 0 == key
+  // 1 == value
+  return pq
+    .toArray()
+    .map((entry) => entry.element[0].repeat(entry.element[1]))
+    .join('');
+};
