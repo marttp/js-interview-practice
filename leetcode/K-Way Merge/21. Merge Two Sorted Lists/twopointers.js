497711ad0dcf7157e6a254994f3ee618785592e9
Thanaphoom Babparn <tp.babparn@gmail.com>
2023-02-04 10:50:17 +0900

    Add 21 solution 2 pointers

---
+++ leetcode/K-Way Merge/21. Merge Two Sorted Lists/twopointers.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function (list1, list2) {
+  if (list1 === null && list2 === null) return null;
+  if (list1 !== null && list2 === null) return list1;
+  if (list1 === null && list2 !== null) return list2;
+
+  const dummy = new ListNode();
+  let current = dummy;
+  while (list1 !== null && list2 !== null) {
+    if (list1.val < list2.val) {
+      current.next = list1;
+      list1 = list1.next;
+    } else {
+      current.next = list2;
+      list2 = list2.next;
+    }
+    current = current.next;
+  }
+  if (list1 !== null) current.next = list1;
+  if (list2 !== null) current.next = list2;
+  return dummy.next;
+};
